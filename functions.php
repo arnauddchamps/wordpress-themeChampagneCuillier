059cb3aa58607a638daa4aa58b19d3c238526de4
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-04-01 21:04:33 +0200

    initialization functions index screenshot style css(bootstrap)

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+//===================================================================================
+//=================== CHARGEMENT DES SCRIPTS
+//===================================================================================
+define('ADCUILLIER_VERSION', '1.0.0');
+
+function adcuillier_scripts() {
+    wp_enqueue_style( 'adcuillier_bootstrap-core', get_template_directory_uri() . '/css/bootstrap.min.css', array(), ADCUILLIER_VERSION , 'all' );
+    wp_enqueue_style( 'adcuillier_custom', get_template_directory_uri() . '/style.css', array('adcuillier_bootstrap-core'), ADCUILLIER_VERSION , 'all' );
+
+
+} //fin function adcuillier_scripts
+
+add_action ('wp_enqueue_scripts', 'adcuillier_scripts');
+
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Champagne Cuillier</title>
+
+    <?php wp_head();  ?>
+
+</head>
+<body>
+    <h1>Coucou c'est moi</h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae id voluptates rerum corporis numquam sed voluptatibus inventore similique, magnam quis non asperiores alias reiciendis, illo suscipit soluta expedita? Impedit!
+    </p>
+    
+</body>
+</html>
