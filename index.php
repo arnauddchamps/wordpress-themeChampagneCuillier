0921265452fb705f97213d48e30808970e018e66
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-04-02 11:52:36 +0200

    add 5 div to section and mb-5

--- index.php
+++ index.php
@@ -11,16 +11,66 @@
 </head>
 <body>
     <div class="container">
-        <div class="row">
-            <div class="col-4">
-                <img src="<?php echo get_template_directory_uri(); ?>/SCRUM.png" alt="test1" class="img-fluid">
-                
-            </div>
-            <div class="col-6">
-                <h1>Coucou c'est moi</h1>    
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae id voluptates rerum corporis numquam sed voluptatibus inventore similique, magnam quis non asperiores alias reiciendis, illo suscipit soluta expedita? Impedit!</p>
-            </div>
+        <div class="jumbotron">
+            <h1>Coucou c'est Vincent</h1>
         </div>
+
+        <section>
+            <div class="container">
+                <div class="row mb-5">
+                    <div class="col-4">
+                        <img src="<?php echo get_template_directory_uri(); ?>/SCRUM.png" alt="test1" class="img-fluid">
+                
+                    </div>
+                    <div class="col-6">
+                        <h1>Coucou c'est moi</h1>    
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae id voluptates rerum corporis numquam sed voluptatibus inventore similique, magnam quis non asperiores alias reiciendis, illo suscipit soluta expedita? Impedit!</p>
+                    </div>
+                </div>
+                <div class="row mb-5">
+                    <div class="col-4">
+                        <img src="<?php echo get_template_directory_uri(); ?>/SCRUM.png" alt="test1" class="img-fluid">
+                
+                    </div>
+                    <div class="col-6">
+                        <h1>Coucou c'est moi</h1>    
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae id voluptates rerum corporis numquam sed voluptatibus inventore similique, magnam quis non asperiores alias reiciendis, illo suscipit soluta expedita? Impedit!</p>
+                    </div>
+                </div>
+                <div class="row mb-5">
+                    <div class="col-4">
+                        <img src="<?php echo get_template_directory_uri(); ?>/SCRUM.png" alt="test1" class="img-fluid">
+                
+                    </div>
+                    <div class="col-6">
+                        <h1>Coucou c'est moi</h1>    
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae id voluptates rerum corporis numquam sed voluptatibus inventore similique, magnam quis non asperiores alias reiciendis, illo suscipit soluta expedita? Impedit!</p>
+                    </div>
+                </div>
+                <div class="row mb-5">
+                    <div class="col-4">
+                        <img src="<?php echo get_template_directory_uri(); ?>/SCRUM.png" alt="test1" class="img-fluid">
+                
+                    </div>
+                    <div class="col-6">
+                        <h1>Coucou c'est moi</h1>    
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae id voluptates rerum corporis numquam sed voluptatibus inventore similique, magnam quis non asperiores alias reiciendis, illo suscipit soluta expedita? Impedit!</p>
+                    </div>
+                </div>
+                <div class="row mb-5">
+                    <div class="col-4">
+                        <img src="<?php echo get_template_directory_uri(); ?>/SCRUM.png" alt="test1" class="img-fluid">
+                
+                    </div>
+                    <div class="col-6">
+                        <h1>Coucou c'est moi</h1>    
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. At quae id voluptates rerum corporis numquam sed voluptatibus inventore similique, magnam quis non asperiores alias reiciendis, illo suscipit soluta expedita? Impedit!</p>
+                    </div>
+                </div>
+                
+            </div>    
+        </section>
+        
     </div>
 
     
