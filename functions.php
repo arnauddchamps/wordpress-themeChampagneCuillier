c295255cedb2d8121a93cc10de0f822ec476007e
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-04-02 08:19:54 +0200

    modif name mach_version & adcuillier_admin

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ function adcuillier_scripts() {
     wp_enqueue_style( 'adcuillier_bootstrap-core', get_template_directory_uri() . '/css/bootstrap.min.css', array(), ADCUILLIER_VERSION , 'all' );
     wp_enqueue_style( 'adcuillier_custom', get_template_directory_uri() . '/style.css', array('adcuillier_bootstrap-core'), ADCUILLIER_VERSION , 'all' );
     //chargement des scripts
-    wp_enqueue_script( 'adcuillier_script', get_template_directory_uri() . '/js/adcuillier.js', array('jquery'), ADCUILLIER_VERSION , true );
+    wp_enqueue_admin_script( 'adcuillier_script', get_template_directory_uri() . '/js/adcuillier.js', array('jquery'), ADCUILLIER_VERSION , true );
     
 
 } //fin function adcuillier_scripts
@@ -23,7 +23,7 @@ add_action('wp_enqueue_scripts', 'adcuillier_scripts');
 //chargement dans l'admin
 function adcuillier_admin_scripts() {
     //chargement des styles
-    wp_enqueue_style('bootstrap-adm-core', get_template_directory_uri() . '/css/bootstrap.min.css', array(), MACH_VERSION );
+    wp_enqueue_style('bootstrap-adm-core', get_template_directory_uri() . '/css/bootstrap.min.css', array(), ADCUILLIER_VERSION );
 
 }//fin de la function
 add_action('admin_init', 'adcuillier_admin_scripts');
